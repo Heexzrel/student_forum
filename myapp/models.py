342f46eb74287b8e9cf801a40c1ac3f21d56f8ea
Israel George <georgeisrael18@outlook.com>
2024-05-12 21:53:53 +0100

    add_role_class

--- myapp/models.py
+++ myapp/models.py
@@ -11,6 +11,11 @@ roles_users = db.Table(
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
         )
 
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
